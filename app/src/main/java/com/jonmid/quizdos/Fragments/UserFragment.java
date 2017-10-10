12e4cd86e35bc067e3589e56eb5b7767b3668fcf
martinez <martinez92108@gmail.com>
2017-10-10 14:06:23 -0500

    1

--- app/src/main/java/com/jonmid/quizdos/Fragments/UserFragment.java
+++ app/src/main/java/com/jonmid/quizdos/Fragments/UserFragment.java
@@ -3,16 +3,25 @@ package com.jonmid.quizdos.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.jonmid.quizdos.Model.UserModelMrtinezJonatan;
 import com.jonmid.quizdos.R;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class UserFragment extends Fragment {
+
+
+    RecyclerView recyclerView;
+    List<UserModelMrtinezJonatan> urlDetailsList;
+    UserModelMrtinezJonatan adapterUrl;
 
 
 
